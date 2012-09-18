70a86f2be7e271684910ccfeb3ae742d5e8490ee
Payal Gupta <payalgupta1985@gmail.com>
2012-09-18 19:14:27 +0530

    initial commit with the rails app

---
+++ apistagram/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apistagram::Application.config.secret_token = '4437797d8c7a6c0b573b5ac5bc0b570d86dcc2a9d9b0a324a1949b88b5f898cdfc9db4558d0ff365c312560c53fe5b703610a15bd1c1783b0189d99a214fd769'
