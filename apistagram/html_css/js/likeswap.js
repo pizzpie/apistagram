f1ad94e0df6b7b7080526915c9ed82ce8667b144
Craig Morrison <craig@craigmorrison.ca>
2012-09-20 23:18:50 -0400

    Updated HTML/CSS (Sept-20)

---
+++ apistagram/html_css/js/likeswap.js
@@ -0,0 +1,51 @@
+// Swapping the heart button under the single image
+
+		function changeImage() {
+    if(document.getElementById('myImage').src == 'http://www.craigmorrison.ca/tatagram/images/heart-icon-off.jpg') {
+        document.getElementById('myImage').src = 'http://www.craigmorrison.ca/tatagram/images/heart-icon-on.jpg';
+    } else if(document.getElementById('myImage').src == 'http://www.craigmorrison.ca/tatagram/images/heart-icon-on.jpg') {
+        document.getElementById('myImage').src = 'http://www.craigmorrison.ca/tatagram/images/heart-icon-off.jpg';
+    }
+    
+
+}
+
+// Swapping the large like button on the single image screen
+
+		function changeLikeButton() {
+    if(document.getElementById('myLikeButton').src == 'http://www.craigmorrison.ca/tatagram/images/like-button-off.gif') {
+        document.getElementById('myLikeButton').src = 'http://www.craigmorrison.ca/tatagram/images/like-button-on.gif';
+    } else if(document.getElementById('myLikeButton').src == 'http://www.craigmorrison.ca/tatagram/images/like-button-on.gif') {
+        document.getElementById('myLikeButton').src = 'http://www.craigmorrison.ca/tatagram/images/like-button-off.gif';
+    }
+    
+
+}
+
+
+// Swapping the Index like image in the thumb
+
+		function changeLikeIndex() {
+    if(document.getElementById('myLikeIndex').src == 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon.jpg') {
+        document.getElementById('myLikeIndex').src = 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon-on.jpg';
+    } else if(document.getElementById('myLikeIndex').src == 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon-on.jpg') {
+        document.getElementById('myLikeIndex').src = 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon.jpg';
+    }
+    
+
+}
+
+
+// Swapping the User Profile like image in the thumb
+
+		function changeLikeUser() {
+    if(document.getElementById('myLikeUser').src == 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon.jpg') {
+        document.getElementById('myLikeUser').src = 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon-on.jpg';
+    } else if(document.getElementById('myLikeUser').src == 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon-on.jpg') {
+        document.getElementById('myLikeUser').src = 'http://www.craigmorrison.ca/tatagram/images/like-stat-icon.jpg';
+    }
+    
+
+}
+
+
