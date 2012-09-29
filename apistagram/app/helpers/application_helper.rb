c5ecba6f18326764c25bb565cacb9bd37a8c0988
Payal Gupta <payalgupta1985@gmail.com>
2012-09-30 02:30:06 +0530

    added fan count for like heart

--- apistagram/app/helpers/application_helper.rb
+++ apistagram/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def heart_link(like_icon, count)
+    image_tag(like_icon, :id => 'myLikeIndex', :class => 'imgmiddle', :alt => 'like-stat-icon') +
+    content_tag(:span, count)
+  end
 end
