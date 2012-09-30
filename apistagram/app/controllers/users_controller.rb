b7051627b94965ac70d44adf7cfbbd28f9170381
Payal Gupta <payalgupta1985@gmail.com>
2012-09-30 11:54:36 +0530

    added the like link changes for non-logged in users

--- apistagram/app/controllers/users_controller.rb
+++ apistagram/app/controllers/users_controller.rb
@@ -1,2 +1,71 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @iphotos }
+    end
+  end
+
+  def show
+    @iphoto = Iphoto.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @iphoto }
+    end
+  end
+
+  def new
+    @iphoto = Iphoto.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @iphoto }
+    end
+  end
+
+  def edit
+    @iphoto = Iphoto.find(params[:id])
+  end
+
+  def create
+    @iphoto = Iphoto.new(params[:iphoto])
+
+    respond_to do |format|
+      if @iphoto.save
+        format.html { redirect_to @iphoto, notice: 'Iphoto was successfully created.' }
+        format.json { render json: @iphoto, status: :created, location: @iphoto }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @iphoto.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @iphoto = Iphoto.find(params[:id])
+
+    respond_to do |format|
+      if @iphoto.update_attributes(params[:iphoto])
+        format.html { redirect_to @iphoto, notice: 'Iphoto was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @iphoto.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @iphoto = Iphoto.find(params[:id])
+    @iphoto.destroy
+
+    respond_to do |format|
+      format.html { redirect_to iphotos_url }
+      format.json { head :no_content }
+    end
+  end
 end
--- apistagram/app/helpers/application_helper.rb
+++ apistagram/app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
-  def heart_link(like_icon, count)
-    image_tag(like_icon, :id => 'myLikeIndex', :class => 'imgmiddle', :alt => 'like-stat-icon') +
-    content_tag(:span, count)
+  def heart_link(iphoto, c_user)
+    if c_user
+      like_icon = c_user.likes?(iphoto) ? 'like-stat-icon-on.jpg' : 'like-stat-icon.jpg'
+      link_to(image_tag(like_icon, :id => 'myLikeIndex', :class => 'imgmiddle', :alt => 'like-stat-icon') +
+      content_tag(:span, iphoto.fans.count), favorite_path(iphoto.id), :title=>"Click to like image", 'data-remote' => true)
+    else
+      like_icon = 'like-stat-icon.jpg'
+      image_tag(like_icon, :id => 'myLikeIndex', :class => 'imgmiddle', :alt => 'like-stat-icon') +
+      content_tag(:span, count)
+    end
   end
 end
