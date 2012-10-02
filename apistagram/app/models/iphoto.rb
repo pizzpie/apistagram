8d20ac64c76625a476f2b6652798c26aecbf765e
Payal Gupta <payalgupta1985@gmail.com>
2012-10-02 20:39:34 +0530

    commente dout validations

--- apistagram/app/models/iphoto.rb
+++ apistagram/app/models/iphoto.rb
@@ -4,11 +4,11 @@ class Iphoto < ActiveRecord::Base
   
   attr_accessible :i_id, :status, :tag_id, :url, :username
 
-  validates :i_id, :url, :username,
-            :presence => true
+  # validates :i_id, :url, :username,
+  #           :presence => true
 
-  validates :i_id,
-            :uniqueness => true
+  # validates :i_id,
+  #           :uniqueness => true
 
   scope :pending, where("status is false")
   scope :selected, where("status is true")
