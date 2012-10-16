752f43c2855c6741db20f31759f0e5a518e0cac1
Payal Gupta <payalgupta1985@gmail.com>
2012-10-16 23:22:09 +0530

    more fixes

--- apistagram/app/controllers/iphotos_controller.rb
+++ apistagram/app/controllers/iphotos_controller.rb
@@ -24,7 +24,7 @@ class IphotosController < ApplicationController
 
   def destroy
     @iphoto = Iphoto.find_by_id(params[:id])
-    if current_user.is_admin? || current_user.name == @iphoto.username 
+    if current_user and current_user.is_admin? || current_user.name == @iphoto.username 
       @iphoto.destroy 
       redirect_to iphotos_url, :notice => "Photo removed successfully!"
     else
