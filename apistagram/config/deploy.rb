856a642ecf6c2b48d04337887e8cac6b64a5c85f
Payal Gupta <payalgupta1985@gmail.com>
2012-10-24 16:12:54 +0530

    changes to deploy file

--- apistagram/config/deploy.rb
+++ apistagram/config/deploy.rb
@@ -29,7 +29,7 @@ namespace :deploy do
   task :stop do ; end
 
   task :bundle_gems do
-    run "cd #{deploy_to}/current && bundle install vendor/gems"
+    run "cd #{deploy_to}/current/apistagram && bundle install vendor/gems"
   end
 
   desc "Restarting Passenger with restart.txt"
