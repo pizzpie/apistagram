f1bdec76a01a3b58b53fcb976296b557f6dc40f3
Payal Gupta <payalgupta1985@gmail.com>
2012-10-24 17:30:08 +0530

    changes to deploy file

--- apistagram/config/deploy.rb
+++ apistagram/config/deploy.rb
@@ -35,11 +35,9 @@ namespace :deploy do
 
   desc "Symlink shared resources on each release - not used"
   task :symlink_shared, :roles => :app do
-    release_dir = "#{release_path}/apistagram"#File.join(release_path, 'apistagram')
-    current_dir = "#{current_release}/apistagram"#File.join(current_release, 'apistagram')
-    # run "ln -nfs #{shared_path}/database.yml #{release_path}/apistagram/config/database.yml"
-    # run "cd #{current_dir} && rake RAILS_ENV=#{rails_env} RAILS_GROUPS=assets assets:precompile"
-    # run "cd #{release_dir} && sudo bundle install --without test development"
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/apistagram/config/database.yml"
+    run "cd #{release_path}/apistagram && sudo bundle install --without test development"
+    run "cd #{release_path}/apistagram && rake RAILS_ENV=#{rails_env} RAILS_GROUPS=assets assets:precompile"
 
     # migrate
   end 
